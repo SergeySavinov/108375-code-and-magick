272c44de1d5eddc9d46eefd4442256b92233ba1c
Sergey Savinov <savinov.s@mail.ru>
2015-11-19 18:06:27 +0300

    Модуль 1 задание 2 правки

--- js/check.js
+++ js/check.js
@@ -7,17 +7,17 @@ function getMessage(a, b) {
     }
   } else if (typeof a == 'number') {
     return 'Я прыгнул на ' + (a * 100) + ' сантиметров';
+  } else if (typeof a =='object' && typeof b == 'object') {
+    var lengthOfPath = 0;
+    for (var i =0; i < a.length; i++) {
+      lengthOfPath += a[i] * b[i];
+    }
+    return 'Я прошёл ' + lengthOfPath + ' метров';
   } else if (typeof a == 'object') {
     var sum = 0;
     for (var i = 0; i < a.length; i++) {
       sum += a[i];
     }
     return 'Я прошёл ' + sum + ' шагов';
-  } else if (typeof b =='object' && typeof a == 'object') {
-    var lengthOfPath = 0;
-    for (var i =0; i < a.length; i++) {
-      lengthOfPath += a[i] * b[i];
-    }
-    return 'Я прошёл ' + lengthOfPath + ' метров';
-  }
+  } 
 }
